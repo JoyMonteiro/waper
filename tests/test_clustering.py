feat: use weighted centroids for cluster representatives and fix distance scaling

tests/test_clustering.py
@@ -5,7 +5,7 @@ import xarray as xr
 from waper.identification import max_min, topology
 
 
-def _create_and_process_field(v, lons, lats, threshold=5):
+def _create_and_process_field(v, lons, lats, threshold=5, eps_km=500):
     da = xr.DataArray(
         v,
         dims=["latitude", "longitude"],
@@ -16,7 +16,7 @@ def _create_and_process_field(v, lons, lats, threshold=5):
     clipped = max_min.clip_dataset(data_with_max, "v", threshold=threshold)
     connectivity = topology.identify_connected_regions(clipped)
     maxima_points = max_min.extract_maxima_points(connectivity, threshold, "v")
-    clustered = topology.cluster_extrema(data_with_max, connectivity, maxima_points, "v", sign=1)
+    clustered = topology.cluster_extrema(data_with_max, connectivity, maxima_points, "v", sign=1, eps_km=eps_km)
     return clustered
 
 
@@ -75,6 +75,44 @@ def test_two_distant_extrema_different_clusters():
     assert len(clusters) > 1
 
 
+def test_centroid_representative():
+    lons = np.arange(0, 360, 5)
+    lats = np.arange(20, 80.1, 5)
+    lon2d, lat2d = np.meshgrid(lons, lats)
+
+    # Asymmetric cluster connected by a broad base
+    base = 15 * np.exp(-((lon2d - 185) ** 2 + (lat2d - 50) ** 2) / 500)
+    # Main peak at lon=180, lat=50, value=30
+    v1 = 30 * np.exp(-((lon2d - 180) ** 2 + (lat2d - 50) ** 2) / 10)
+    # Secondary peak at lon=190, lat=50, value=20
+    v2 = 20 * np.exp(-((lon2d - 190) ** 2 + (lat2d - 50) ** 2) / 10)
+    
+    # They will fuse into one cluster (eps=1000km covers 10 deg at lat=50)
+    # The base ensures they are in the same connected region > threshold (5)
+    v = base + v1 + v2
+
+    clustered = _create_and_process_field(v, lons, lats, threshold=5, eps_km=1000)
+    
+    # Check max_cluster_assign
+    (
+        cluster_max_arr,
+        cluster_max_point,
+        max_pt_dict,
+        num_max_clusters,
+    ) = topology.max_cluster_assign(clustered, "v")
+
+    assert num_max_clusters == 1
+    
+    # The absolute peak is at lon=180, but the centroid should be pulled towards 190
+    centroid_lon = cluster_max_point[0][0]
+    
+    # It must be strictly greater than 180.0
+    assert centroid_lon > 180.0
+    # It must be strictly less than 190.0
+    assert centroid_lon < 190.0
+    
+    # The max value should still be strictly tracked as the peak (around 30)
+    assert cluster_max_arr[0] >= 30.0
 def test_isolated_outlier_far_from_group():
     lons = np.arange(0, 360, 2.5)
     lats = np.arange(20, 80.1, 2.5)

waper/identification/topology.py
@@ -266,17 +266,44 @@ def min_cluster_assign(min_points, scalar_name):
     cluster_min_arr = np.full(num_min_clusters, 0.0)
     cluster_min_point = np.full((num_min_clusters, 2), 0.0)
     min_scalars = min_points[scalar_name]
+    
+    cluster_lon_sum = np.zeros(num_min_clusters)
+    cluster_lat_sum = np.zeros(num_min_clusters)
+    cluster_weight_sum = np.zeros(num_min_clusters)
+    cluster_base_lon = np.full(num_min_clusters, -1.0)
 
-    # Identify the most negative point in the cluster
     for i in range(num_points_min):
-        x, y = min_points["Longitude"][i], min_points["Latitude"][i]
-        coords = [x, y]
-        min_pt_dict[cluster_id_min[i]].append(coords)
+        cid = cluster_id_min[i]
+        lon = min_points["Longitude"][i]
+        lat = min_points["Latitude"][i]
+        val = min_scalars[i]
+        weight = abs(val)
+        
+        min_pt_dict[cid].append([lon, lat])
 
-        if cluster_min_arr[cluster_id_min[i]] > min_scalars[i]:
-            cluster_min_arr[cluster_id_min[i]] = min_scalars[i]
-            cluster_min_point[cluster_id_min[i]][0] = min_points["Longitude"][i]
-            cluster_min_point[cluster_id_min[i]][1] = min_points["Latitude"][i]
+        if cluster_min_arr[cid] > val:
+            cluster_min_arr[cid] = val
+            
+        if cluster_base_lon[cid] == -1.0:
+            cluster_base_lon[cid] = lon
+            
+        shifted_lon = lon
+        if abs(lon - cluster_base_lon[cid]) > 180:
+            if lon > cluster_base_lon[cid]:
+                shifted_lon -= 360
+            else:
+                shifted_lon += 360
+                
+        cluster_lon_sum[cid] += shifted_lon * weight
+        cluster_lat_sum[cid] += lat * weight
+        cluster_weight_sum[cid] += weight
+
+    for cid in range(num_min_clusters):
+        if cluster_weight_sum[cid] > 0:
+            avg_lon = cluster_lon_sum[cid] / cluster_weight_sum[cid]
+            avg_lat = cluster_lat_sum[cid] / cluster_weight_sum[cid]
+            cluster_min_point[cid][0] = avg_lon % 360
+            cluster_min_point[cid][1] = avg_lat
 
     return (cluster_min_arr, cluster_min_point, min_pt_dict, num_min_clusters)
 
@@ -300,15 +327,43 @@ def max_cluster_assign(max_points, scalar_name):
     cluster_max_arr = np.full(num_max_clusters, 0.0)
     cluster_max_point = np.full((num_max_clusters, 2), 0.0)
     max_scalars = max_points[scalar_name]
+    
+    cluster_lon_sum = np.zeros(num_max_clusters)
+    cluster_lat_sum = np.zeros(num_max_clusters)
+    cluster_weight_sum = np.zeros(num_max_clusters)
+    cluster_base_lon = np.full(num_max_clusters, -1.0)
 
-    # Identify largest point in each cluster
     for i in range(num_points_max):
-        x, y = max_points["Longitude"][i], max_points["Latitude"][i]
-        coords = [x, y]
-        max_pt_dict[cluster_id_max[i]].append(coords)
-        if cluster_max_arr[cluster_id_max[i]] < max_scalars[i]:
-            cluster_max_arr[cluster_id_max[i]] = max_scalars[i]
-            cluster_max_point[cluster_id_max[i]][0] = max_points["Longitude"][i]
-            cluster_max_point[cluster_id_max[i]][1] = max_points["Latitude"][i]
+        cid = cluster_id_max[i]
+        lon = max_points["Longitude"][i]
+        lat = max_points["Latitude"][i]
+        val = max_scalars[i]
+        
+        max_pt_dict[cid].append([lon, lat])
+        
+        if cluster_max_arr[cid] < val:
+            cluster_max_arr[cid] = val
+            
+        if cluster_base_lon[cid] == -1.0:
+            cluster_base_lon[cid] = lon
+            
+        # Shift longitude if it wraps around
+        shifted_lon = lon
+        if abs(lon - cluster_base_lon[cid]) > 180:
+            if lon > cluster_base_lon[cid]:
+                shifted_lon -= 360
+            else:
+                shifted_lon += 360
+                
+        cluster_lon_sum[cid] += shifted_lon * val
+        cluster_lat_sum[cid] += lat * val
+        cluster_weight_sum[cid] += val
+
+    for cid in range(num_max_clusters):
+        if cluster_weight_sum[cid] > 0:
+            avg_lon = cluster_lon_sum[cid] / cluster_weight_sum[cid]
+            avg_lat = cluster_lat_sum[cid] / cluster_weight_sum[cid]
+            cluster_max_point[cid][0] = avg_lon % 360
+            cluster_max_point[cid][1] = avg_lat
 
     return (cluster_max_arr, cluster_max_point, max_pt_dict, num_max_clusters)